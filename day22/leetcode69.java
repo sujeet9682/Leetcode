babb31260a18c0658810bf6995b478d725a407e9
Sujeet Kumar <sujitg0124@gmail.com>
2023-10-12 23:08:42 +0530

    sqrt via binary search

---
+++ day22/leetcode69.java
@@ -0,0 +1,27 @@
+class leetcode69 {
+    public int mySqrt(int x){
+        return (int)bin(x,0,x/2);
+    }
+    public long bin(long key, long first, long last)
+    {
+        long mid = first-(first-last)/2;
+        if(key == first*first)
+            return first;
+        else if(key == last*last)
+            return last;
+        if(key>mid*mid)
+        {
+            if((mid+1)*(mid+1)>key)
+                return mid;
+            else
+                first = mid+1;
+        }
+        else if(key<(mid*mid))
+        {
+            last = mid-1;
+        }
+        else if(key == mid*mid || last<first)
+            return mid;
+        return bin(key, first, last);
+    }
+}
