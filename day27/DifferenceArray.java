4f4e94fdf7a31d2dd9a156525c699f929f31493e
Sujeet Kumar <sujitg0124@gmail.com>
2023-10-18 22:33:57 +0530

    using ArrayList methods

---
+++ day27/DifferenceArray.java
@@ -0,0 +1,34 @@
+package day27;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DifferenceArray {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+         List<List<Integer>> li = new ArrayList<>();
+        Set<Integer> s1 = new HashSet<>();
+        Set<Integer> s2 = new HashSet<>();
+        Set<Integer> s3 = new HashSet<>();
+
+
+        for (int num : nums1) {
+            s1.add(num);
+        }
+        for (int num : nums2) {
+            s3.add(num);
+        }
+
+        for (int num : nums2) {
+            if (!s1.contains(num)) {
+                s2.add(num);
+            }
+        }
+
+        s1.removeAll(s3);
+        li.add(new ArrayList<Integer>(s1));
+        li.add(new ArrayList<Integer>(s2));
+       return li;
+    }
+}
